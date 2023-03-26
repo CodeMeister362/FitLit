d0249b0afeda8c76f4362fd4bbb45d84347e408d
Race Osuna <raceosuna@gmail.com>
2023-03-26 15:15:33 -0600

    Refactor: UserRepoitory class with refactored constructor

--- src/UserRepository.js
+++ src/UserRepository.js
@@ -1,12 +1,12 @@
 class UserRepository {
-  constructor(id, name, address, email, strideLength) {
+  constructor(id, name, address, email, strideLength, dailyStepGoal, friends) {
     this.id = id
     this.name = name
     this.address = address
     this.email = email
     this.strideLength = strideLength
-  //   this.dailyStepGoal = details.dailyStepGoal
-  //   this.friends = details.friends
+    this.dailyStepGoal = dailyStepGoal
+    this.friends = friends
   }
 }
 
