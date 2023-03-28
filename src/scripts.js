310334a6cc414e1e1f9306aca4dff6df084c4aa0
CBradsen <117617970+CBradsen@users.noreply.github.com>
2023-03-27 18:46:25 -0600

    feature:Create and display random user data on the DOM

--- src/scripts.js
+++ src/scripts.js
@@ -14,53 +14,27 @@ console.log('This is the JavaScript entry file - your code begins here.');
 // An example of how you tell webpack to use a JS file
 
 import userData from './data/users';
-console.log("User Data:", userData);
-
 import UserRepository from './UserRepository';
 
-const user = new UserRepository();
-
-const userFirstName = document.querySelector(".user-card");
-const checkRandom = document.querySelector(".welcome");
-let userCard = document.querySelector(".user-card")
-// window.addEventListener("DOMContentLoaded", generateRandomUser);
-window.addEventListener("load", (event) => {
-  userCard.textContent = `Hello ${userData.users[2].name}`
+function getRandomInt() {
+  return Math.floor(Math.random() * 49);
 }
-)
 
-function show() {
-};
-
-function hide() {
-
-};
-
-// function generateRandomIndex(array) {
-//   const arrayIndex = Math.floor(Math.random() * array.length);
-//   return array[arrayIndex];
-
-// };
-
-function generateRandomUser() {
-  // console.log(users)
-// const test = users[generateRandomIndex(users)]
-// console.log(UserData.users[2].name);
-checkRandom.innerHTML += `${UserData.users[2].name}`;
-
-};
+const randomNum = getRandomInt();
 
 
-// function generateRandomUser() {
-//   const usersIndex = Math.floor(Math.random() * 50);
-//   return usersIndex;
+const user = new UserRepository(userData.users[randomNum].id, userData.users[randomNum].name, userData.users[randomNum].address, userData.users[randomNum].email, userData.users[randomNum].strideLength, userData.users[randomNum].dailyStepGoal, userData.users[randomNum].friends)
 
-// };
 
-// function generateRandomUser() {
-//   console.log(users)
-// const test = users[generateRandomIndex()]
-// console.log(test);
-// checkRandom.innerHTML += `${test}`;
+const userCard = document.querySelector('.user-card')
 
-// };
+window.addEventListener("load", (event) => {
+  userCard.innerHTML = 
+ `<h3>Welcome ${user.getFirstName(user.id, userData.users)}!</h3>
+  <ul>
+      <li>Your daily step goal is ${user.dailyStepGoal}</li>
+      <li>The average step goal of all FitLitFans is ${user.getAverageSteps(userData.users)}</li>
+    </ul>`
+});
+
+
