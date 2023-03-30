39f8895b42ac4e46bdfce2a4d73b829cde93f2de
Priscilla Paxton <ppaxton22@turing.edu>
2023-03-30 11:47:08 -0600

    Begin pulling data from api

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -3,3 +3,14 @@
 
 console.log('I will be a fetch request!')
 
+
+export function fetchUsers() {
+  return fetch("https://fitlit-api.herokuapp.com/api/v1/users")
+    .then(response => response.json());
+}
+
+export function fetchHydration() {
+  return fetch("https://fitlit-api.herokuapp.com/api/v1/hydration")
+    .then(response => response.json())
+}
+
--- src/hydrationClass.js
+++ src/hydrationClass.js
@@ -33,3 +33,4 @@ class Water{
 
 export default Water;
 
+
