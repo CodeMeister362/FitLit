356cef5c821e7250c8b1fdf0cde003cabbd6c8aa
CBradsen <117617970+CBradsen@users.noreply.github.com>
2023-03-31 17:41:44 -0600

    feature: Complete Sleep Class and Tests

--- src/sleepClass.js
+++ src/sleepClass.js
@@ -45,35 +45,19 @@ class Sleep{
       hoursWeek.push(item)
     }
    });
-
     return hoursWeek.reduce((acc, item) => ({...acc, [item.date]: item.hoursSlept}), {});
-  //   ({...acc, [item.date]: item.hoursSlept}), {})
-  //  return acc;
-  
+  }
+  getSleepQualitytByWeek(givenID, start, end) {
+   let sleepQualityWeek = []
+   const user = this.data.sampleSleep.filter((item) => item.userID === givenID);
+   user.forEach((item) => {
+    if (item.date >= start && item.date <= end) {
+      sleepQualityWeek.push(item)
+    }
+   });
+    return sleepQualityWeek.reduce((acc, item) => ({...acc, [item.date]: item.sleepQuality}), {});
   }
 };
-
-
-
-//  overAWeek = (data, id, startDate, stopDate) => {
-//       let waterData = []
-//       const user = data.filter((item) => item.userID === id)
-//       user.forEach((item) => {
-//         if (item.date >= startDate && item.date <= stopDate)
-//           waterData.push(item)
-//       })
-//       return waterData.reduce((acc, item) => ({...acc, [item.date]: item.numOunces}),{})
-//     }
-// const averageSleep = (array, userId) => {
-  
-//   let userHoursSlept = array.filter((sleepObject) => sleepObject.userID === userId)
-//   let average = userHoursSlept.reduce((acc, currentDay) => {
-//     console.log(currentDay.hoursSlept)
-//     acc += currentDay.hoursSlept / userHoursSlept.length
-//     return acc
-//   }, 0)
-//      return average                               
-//  }
 
 
 export default Sleep;
--- test/sleepClass-test.js
+++ test/sleepClass-test.js
@@ -36,22 +36,22 @@ let sleepData = new Sleep(sampleSleepData);
 "2023/03/29" : 5.6,
 "2023/03/30" : 6.2,
 })
+  });
+
+     it("should take in a specific day and return the users sleep quality for that date", function() {
+
+  assert.equal(sleepData.getSleepQualityByDay(2, "2023/03/24"), 3.5)
+  })
+
+  it("should return sleep quality for each day over 7 days", function() {
+    assert.deepEqual(sleepData.getSleepQualitytByWeek(1, "2023/03/24", "2023/03/30"), {
+"2023/03/24" : 4.3,
+"2023/03/25" : 3.3,
+"2023/03/26" : 3.1,
+"2023/03/27" : 4.7,
+"2023/03/28" : 4.6,
+"2023/03/29" : 2.1,
+"2023/03/30" : 3.3,
+})
   })
 });
-
-
-// --All Time: ave number of hours slept 
-// --All Time ave quality / day over all time
-// --Specific Day: hours slept 
-// --Specific Day: sleep quality 
-// Week: hours slept
-// Week: sleep quality each day over the course of a given week 
-// {
-// "2023/03/24" : 4.3,
-// "2023/03/25" : 3.3,
-// "2023/03/26" : 3.1,
-// "2023/03/27" : 4.7,
-// "2023/03/28" : 4.6,
-// "2023/03/29" : 2.1,
-// "2023/03/30" : 3.3,
-// }
