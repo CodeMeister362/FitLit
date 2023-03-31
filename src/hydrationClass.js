945d8323a3a23978563ab5e73804ca5f63a9af38
Nathan Chamberlain <chamberlainuxdesign@gmail.com>
2023-03-31 16:36:24 -0700

    Adds updated hydration class that accepts class object

--- src/hydrationClass.js
+++ src/hydrationClass.js
@@ -1,13 +1,9 @@
 class Water{
   constructor(data) {
-    //this.data = data
-    this.id = data.userID
-    this.date = data.date
-    this.numOunces = data.numOunces
+    this.data = data
   }
   averageOuncesPerDay(data, givenID) {
-    console.log('what is this', this.data)
-    let userNumOunces = data.filter((hydrationObject) => hydrationObject.userID === givenID)
+    let userNumOunces = data.hydrationData.filter((hydrationObject) => hydrationObject.userID === givenID)
     let average = userNumOunces.reduce((acc, currentDay) => {
       acc += currentDay.numOunces / userNumOunces.length
       return acc
@@ -16,7 +12,7 @@ class Water{
     return average
   }
   getSpecificDay(data, givenID, givenDay) {
-    const specificDayUser = data.find((hydrationData) => {
+    const specificDayUser = data.hydrationData.find((hydrationData) => {
       return hydrationData.date === givenDay && hydrationData.userID === givenID
     })
     return specificDayUser.numOunces
@@ -24,7 +20,7 @@ class Water{
 
   overAWeek = (data, id, startDate, stopDate) => {
       let waterData = []
-      const user = data.filter((item) => item.userID === id)
+      const user = data.hydrationData.filter((item) => item.userID === id)
       user.forEach((item) => {
         if (item.date >= startDate && item.date <= stopDate)
           waterData.push(item)
