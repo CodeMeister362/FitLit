0678d5dfe3908769a3919e12d71eb8aabe4707b9
Priscilla Paxton <ppaxton22@turing.edu>
2023-03-31 18:08:01 -0600

    complete activity class and tests

--- test/activity-test.js
+++ test/activity-test.js
@@ -5,9 +5,7 @@ describe('activity class', () => {
 
 	let activity;
 	
-
 	beforeEach(() => {
-		 
 	
 			activity = new Activity({
 				"activityData": [
@@ -31,9 +29,51 @@ describe('activity class', () => {
 				"numSteps": 12970,
 				"minutesActive": 282,
 				"flightsOfStairs": 38
-				}
+				},
+				{
+					"userID": 1,
+					"date": "2023/03/24",
+					"numSteps": 7362,
+					"minutesActive": 261,
+					"flightsOfStairs": 26
+					},
+					{
+					"userID": 2,
+					"date": "2023/03/25",
+					"numSteps": 9000,
+					"minutesActive": 125,
+					"flightsOfStairs": 43
+					},
+					{
+					"userID": 3,
+					"date": "2023/03/24",
+					"numSteps": 12970,
+					"minutesActive": 282,
+					"flightsOfStairs": 38
+					},
+					{
+						"userID": 1,
+						"date": "2023/03/24",
+						"numSteps": 7362,
+						"minutesActive": 261,
+						"flightsOfStairs": 26
+						},
+						{
+						"userID": 2,
+						"date": "2023/03/26",
+						"numSteps": 10000,
+						"minutesActive": 125,
+						"flightsOfStairs": 43
+						},
+						{
+						"userID": 3,
+						"date": "2023/03/24",
+						"numSteps": 12970,
+						"minutesActive": 282,
+						"flightsOfStairs": 38
+						}
 			]},
-				{ users: [
+				{ "users": [
 				{
 					"id": 1,
 					"name": "Trystan Gorczany",
@@ -79,20 +119,24 @@ describe('activity class', () => {
 	})
 
 		it('should be a function', () => {
+
 			assert.isFunction(Activity)
 		})
 
 		it('should return miles walked in day', () => {
 
-			activity.getMilesWalked(1, "2023/03/24")
-
-			assert.equal(activity.getMilesWalked(), .46)
+			assert.equal(activity.getMilesWalked(1, "2023/03/24"), .4647727272727273)
 		})
 
 		it('should return minutes active in a day', () => {
 
-			activity.getMinutesActive(2, "2023/03/24")
-
-			assert.equal(activity.getMinutesActive(), 125)
+			assert.equal(activity.getMinutesActive(2, "2023/03/24"), 125)
 		}) 
+
+		it('should tell user if they have met their step goal for the day', () => {
+
+
+			assert.equal(activity.determineReachGoal(2, "2023/03/24"),'You were 5951 steps below your goal of 9000 steps!')
+			assert.equal(activity.determineReachGoal(2, "2023/03/25"),'You hit your step goal! You walked 9000 steps out of your goal of 9000 steps')
+		})
 })
