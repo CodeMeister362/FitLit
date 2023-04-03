dee07948ac48af2e1cb8c2519b0c80ae18a12de2
CBradsen <117617970+CBradsen@users.noreply.github.com>
2023-04-03 17:34:15 -0600

    "fix: Merge differences fixed"

--- test/sleepClass-test.js
+++ test/sleepClass-test.js
@@ -47,7 +47,6 @@ describe("Sleep Class", () => {
     assert.equal(sleepData.getSleepQualityByDay(2, "2023/03/24"), 3.5)
   });
 
-<<<<<<< HEAD
   assert.equal(mockSleep.getSleepQualityByDay(2, "2023/03/24"), 3.5)
   })
 
@@ -61,34 +60,5 @@ it("should return sleep quality for each day over 7 days", function() {
     "2023/03/29" : 2.1,
     "2023/03/30" : 3.3,
     })
-  })
-=======
-  it("should return hours slept for each day over 7 days", () => {
-    assert.deepEqual(sleepData.getHoursSleptByWeek(1, "2023/03/24", "2023/03/30"), {
-      "2023/03/24" : 9.6,
-      "2023/03/25" : 6.3,
-      "2023/03/26" : 5.4,
-      "2023/03/27" : 7.1,
-      "2023/03/28" : 6,
-      "2023/03/29" : 5.6,
-      "2023/03/30" : 6.2,
-    });
   });
 
-  it("should take in a specific day and return the users sleep quality for that date", () => {
-    assert.equal(sleepData.getSleepQualityByDay(2, "2023/03/24"), 3.5)
-    });
-
-  it("should return sleep quality for each day over 7 days", () => {
-    assert.deepEqual(sleepData.getSleepQualitytByWeek(1, "2023/03/24", "2023/03/30"), {
-      "2023/03/24" : 4.3,
-      "2023/03/25" : 3.3,
-      "2023/03/26" : 3.1,
-      "2023/03/27" : 4.7,
-      "2023/03/28" : 4.6,
-      "2023/03/29" : 2.1,
-      "2023/03/30" : 3.3,
-    });
-  });
->>>>>>> main
-});
