00b5823d57988c6f598835c6d8f42db02c1ee9d9
CBradsen <117617970+CBradsen@users.noreply.github.com>
2023-04-14 18:19:06 -0600

    feature: Add two methods to find new user trends

--- src/activityClass.js
+++ src/activityClass.js
@@ -13,8 +13,8 @@ class Activity {
   checkDateExists = (date) => {
     return this.activity.activityData.some((data) => {
       return data.date === date 
-  })
-}
+    })
+  }
 
   getMilesWalked = (givenId, date) => {
     if(this.checkDateExists(date)) {
@@ -28,7 +28,7 @@ class Activity {
     } else {
     // console.log('this day doesnt exist!')
     return 'this day doesnt exist!'
-  }
+    }
   }
 
   getMinutesActive = (givenId, date) => {
@@ -76,7 +76,61 @@ class Activity {
     } else {
       return 'wrongDates!'
     }
-  };
-}
+  }
 
+  getAllMilesWalked = (givenId) => {
+   const specificUser = this.data.users.find((user) => user.id === givenId)
+   const specificUserStrideLength = specificUser.strideLength
+   const usersSteps = this.activity.activityData.filter(day => day.userID === givenId ) 
+   const allSteps = usersSteps.reduce((acc, day) => {
+            acc += day.numSteps         
+      return acc
+    }, 0)  
+    return (Math.round((allSteps * specificUserStrideLength / 63360) * 10) / 10);
+    } 
+  
+  getPercentGoalsMet = (givenId) => {
+      const specificUser = this.data.users.find((user) => {
+        return user.id === givenId
+      }) 
+      const specificUserStepGoal = specificUser.dailyStepGoal
+      console.log(specificUserStepGoal)
+      const usersData = this.activity.activityData.filter(day => day.userID === givenId ) 
+      console.log(usersData)
+      const totalDays = usersData.length;
+      console.log(totalDays)
+
+      const daysMet = usersData.reduce((acc, day) => {
+        if ( day.numSteps >= specificUserStepGoal) {
+          acc += 1
+      }
+      return acc
+   }, 0)
+
+  const percentMet = (daysMet / totalDays) * 100; 
+  return(`In the last ${totalDays} days you've met your step goal ${percentMet.toFixed(0)}% of the time and walked `);
+  
+   
+  }
+
+
+};
 export default Activity;
+
+// determineReachGoal = (givenId, date) => {
+//     if(this.checkDateExists(date)) {
+//       const specificUser = this.data.users.find((user) => {
+//         return user.id === givenId
+//       }) 
+//       const specificUserStepGoal = specificUser.dailyStepGoal
+//       const specificDaySteps = this.getSpecificDayData(givenId, date).numSteps
+
+//       if(specificUserStepGoal > specificDaySteps) {
+//         return `You walked ${specificDaySteps} steps today. That's ${specificUserStepGoal - specificDaySteps} steps below your goal of ${specificUserStepGoal} steps.`
+//       } else if(specificDaySteps > specificUserStepGoal) {
+//         return (`You walked ${specificDaySteps} steps today. Thats ${specificDaySteps - specificUserStepGoal} steps above your goal of ${specificUserStepGoal} steps!`)
+//       }
+//     } else {
+//       return 'this day doesnt exist!'
+//     }
+//   };
